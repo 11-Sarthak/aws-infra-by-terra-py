8f9b5dc600eba779246579303a6d5258c3402818
richardfrayson07 <sarthakjr14@gmail.com>
2025-02-19 15:57:00 +0530

    Initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+f"terraform -chdir={directory} plan"
---
+++ terra.py
@@ -0,0 +1,21 @@
+import subprocess
+def terraform_run(command):    
+    process=subprocess.run(command, shell=True,check=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    return process.stdout.decode()
+         
+              
+
+
+directory="C:/Users/sarth/Downloads/aws/Wanderlust-Mega-Project/terraform"
+command=f'terraform -chdir={directory} init'
+command1=f'terraform -chdir={directory} plan'
+command2=f'terraform -chdir={directory} apply -auto-approve'
+command3=f'terraform -chdir={directory} destroy -auto-approve'
+
+
+
+
+output=terraform_run("command you want to enter")
+print(output)
+
+ 
